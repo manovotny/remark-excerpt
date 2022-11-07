38be515523f08d54fb5c78c7299be52709129a0b
Michael Novotny <manovotny@gmail.com>
2022-11-06 23:21:33 -0600

    Removes commented out code.

--- example/example.js
+++ example/example.js
@@ -3,10 +3,8 @@ import {toVFile} from 'to-vfile';
 
 import plugin from '../index.js';
 
-// (async () => {
 const file = await toVFile.read('./example/example.md');
 const result = await remark().use(plugin).process(file);
 
 // eslint-disable-next-line no-console
 console.log(result.toString());
-// })();
